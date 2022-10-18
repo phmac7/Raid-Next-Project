44f2e87f30911e2d5ed1639074347d9f62eb3dd6
Pedro Machado <pedro.machado@valtech.com>
2022-10-18 17:47:33 -0300

    import css and ThemeProvider from next-themes lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import "../styles/globals.scss";
+import "@/styles/globals.scss";
+import "@/styles/variables.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider defaultTheme="dark">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
