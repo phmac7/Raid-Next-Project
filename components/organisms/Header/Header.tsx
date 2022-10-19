dd36bf9c10f5dc7deb909d22d60c2dda2dd1e302
John Mello <johncharlesmello@gmail.com>
2022-10-19 18:18:20 -0300

    create Header component

--- components/organisms/Header/Header.tsx
+++ components/organisms/Header/Header.tsx
@@ -1,4 +1,39 @@
-const Header = () => {
-  return <div>Header</div>;
+import LogoDefault from '@/components/atoms/Icons/Logo/LogoDefault';
+import Image from 'next/image';
+import { FC } from 'react';
+
+import styles from './header.module.scss';
+
+interface HeaderProps {
+  userName: string;
+  userPicturePath: string;
+}
+
+const Header: FC<HeaderProps> = ({ userName, userPicturePath }) => {
+  return (
+    <nav className={styles.header}>
+      <div className={styles['header__logo']}>
+        <LogoDefault width="195" height="40" />
+      </div>
+      <div>
+        <input
+          className={styles['header__searchInput']}
+          type="text"
+          name="searchInput"
+          id="searchInput"
+        />
+      </div>
+      <div className={styles['header__userInfo']}>
+        <span className={styles['header__userName']}>{userName}</span>
+        <Image
+          className={styles['header__userPicture']}
+          src={userPicturePath}
+          alt={`picture of ${userName}`}
+          width="60px"
+          height="60px"
+        />
+      </div>
+    </nav>
+  );
 };
 export default Header;
