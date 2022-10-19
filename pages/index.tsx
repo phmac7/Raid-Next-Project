1fb5574b5c9194eab64713bb4a3267cd7ecb8d27
John Mello <johncharlesmello@gmail.com>
2022-10-19 18:24:59 -0300

    creat initial mockUser, add Header to Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
-import { ThemeChanger, Button } from "@/components/atoms";
-import type { NextPage } from "next";
-import styles from "@/styles/pages/Home.module.scss";
+import { Header } from '@/components/organisms';
+import type { NextPage } from 'next';
+
+const mockUser = {
+  name: 'Mussum Cacildis',
+  picture:
+    'https://s2.glbimg.com/U7TAp42IcAp-o-2tTFI5HBJbUvA=/0x0:1024x1024/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/t/P/kBI0LWRUemtdGTdIi9iQ/mussumdivulgacao.jpg',
+};
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1 className={styles.home__title}>Hello World</h1>
-      <ThemeChanger />
-      <Button label="Button" />
+      <Header userName={mockUser.name} userPicturePath={mockUser.picture} />
     </>
   );
 };
