57bdab32df9fc4b57d8d50f19eaf7e3f67f3a888
John Mello <johncharlesmello@gmail.com>
2022-10-19 18:15:32 -0300

    change file extension from .ts to .tsx

---
+++ components/organisms/Header/Header.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Header from './Header';
+export default {
+  title: 'Component/Header',
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  userName: 'Mussum Cacilds',
+  userPicturePath:
+    'https://s2.glbimg.com/U7TAp42IcAp-o-2tTFI5HBJbUvA=/0x0:1024x1024/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/t/P/kBI0LWRUemtdGTdIi9iQ/mussumdivulgacao.jpg',
+};
