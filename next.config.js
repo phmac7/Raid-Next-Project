98be90f20bf3ef0040b9ff06998ad2871ce788d9
John Mello <johncharlesmello@gmail.com>
2022-10-20 18:50:01 -0300

    add config for outsourced mock user image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's2.glbimg.com',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
