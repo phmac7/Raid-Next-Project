d1c274b20768280c796456abb534218c5f58d14e
KenjiWorkES <lucas.kenjies@gmail.com>
2022-10-25 15:15:13 -0300

    create api route to all elos

---
+++ pages/api/elo/index.tsx
@@ -0,0 +1,18 @@
+import {
+  createContentfulClient,
+  getAllEntriesFromOneContent,
+} from '../../../helpers/contentiful';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const client = createContentfulClient();
+
+  if (req.method === 'GET') {
+    const response = await getAllEntriesFromOneContent(client, 'elo');
+    res.status(200).json({ items: response });
+  }
+}
