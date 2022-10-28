c8eef36c9fc23152e7f0f1e61d4683ec12eb1968
KenjiWorkES <lucas.kenjies@gmail.com>
2022-10-28 10:14:40 -0300

    create stories, css file and tsx file structure to AvatarInput

---
+++ components/atoms/AvatarInput/AvatarInput.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import AvatarInput from './AvatarInput';
+
+export default {
+  title: 'Component/AvatarInput',
+  component: AvatarInput,
+} as Meta;
+
+export const Default: StoryObj = {};
---
+++ components/atoms/AvatarInput/AvatarInput.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './AvatarInput.module.scss';
+
+const AvatarInput = () => {
+  return <div className={styles.avatarInput}></div>;
+};
+
+export default AvatarInput;
