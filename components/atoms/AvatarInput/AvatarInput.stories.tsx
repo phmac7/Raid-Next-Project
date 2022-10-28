9dbc52a1b83ffcdbe2957c40fb266fcd948e9da7
KenjiWorkES <lucas.kenjies@gmail.com>
2022-10-28 10:23:48 -0300

    create all tags and types inside AvatarInput

--- components/atoms/AvatarInput/AvatarInput.stories.tsx
+++ components/atoms/AvatarInput/AvatarInput.stories.tsx
@@ -1,9 +1,15 @@
 import { Meta, StoryObj } from '@storybook/react';
-import AvatarInput from './AvatarInput';
+import AvatarInput, { AvatarInputProps } from './AvatarInput';
 
 export default {
   title: 'Component/AvatarInput',
   component: AvatarInput,
-} as Meta;
+  args: {
+    placeholder: "What's is happening",
+    avatar:
+      'https://s2.glbimg.com/U7TAp42IcAp-o-2tTFI5HBJbUvA=/0x0:1024x1024/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/t/P/kBI0LWRUemtdGTdIi9iQ/mussumdivulgacao.jpg',
+    id: 'createPost',
+  },
+} as Meta<AvatarInputProps>;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj<AvatarInputProps> = {};
--- components/atoms/AvatarInput/AvatarInput.tsx
+++ components/atoms/AvatarInput/AvatarInput.tsx
@@ -1,8 +1,36 @@
+import Image from 'next/image';
 import React from 'react';
 import styles from './AvatarInput.module.scss';
 
-const AvatarInput = () => {
-  return <div className={styles.avatarInput}></div>;
+export interface AvatarInputProps {
+  id: string;
+  placeholder: string;
+  avatar: string;
+}
+
+const AvatarInput: React.FC<AvatarInputProps> = ({
+  id,
+  placeholder,
+  avatar,
+}) => {
+  return (
+    <div className={styles.avatarInput}>
+      <Image
+        className={styles['avatarInput__avatar']}
+        src={avatar}
+        alt=""
+        height={50}
+        width={50}
+      />
+      <label className={styles['avatarInput__label']} htmlFor={id}></label>
+      <input
+        className={styles['avatarInput__field']}
+        id={id}
+        placeholder={placeholder}
+        type="text"
+      />
+    </div>
+  );
 };
 
 export default AvatarInput;
