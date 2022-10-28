33cc1576832614e66a62c15d48a9acde9e552a41
KenjiWorkES <lucas.kenjies@gmail.com>
2022-10-28 10:39:28 -0300

    add style to AvatarInput

--- components/atoms/AvatarInput/AvatarInput.stories.tsx
+++ components/atoms/AvatarInput/AvatarInput.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import AvatarInput, { AvatarInputProps } from './AvatarInput';
+import styles from './AvatarInput.module.scss';
 
 export default {
   title: 'Component/AvatarInput',
@@ -10,6 +11,13 @@ export default {
       'https://s2.glbimg.com/U7TAp42IcAp-o-2tTFI5HBJbUvA=/0x0:1024x1024/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/t/P/kBI0LWRUemtdGTdIi9iQ/mussumdivulgacao.jpg',
     id: 'createPost',
   },
+  decorators: [
+    (Story) => (
+      <div className={styles['avatarInput__decorator']}>
+        <Story />
+      </div>
+    ),
+  ],
 } as Meta<AvatarInputProps>;
 
 export const Default: StoryObj<AvatarInputProps> = {};
