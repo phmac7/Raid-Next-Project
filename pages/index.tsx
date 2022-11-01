82c249ae2fc53ed73a5f31f0deb73fc6fc969752
KenjiWorkES <lucas.kenjies@gmail.com>
2022-11-01 13:39:27 -0300

    resolve conflict in homePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,5 @@
 import { Feed } from '@/components/organisms';
 import type { NextPage } from 'next';
-<<<<<<< HEAD
-import { useEffect } from 'react';
-
-const mockUser = {
-  name: 'Mussum Cacildis',
-  picture:
-    'https://s2.glbimg.com/U7TAp42IcAp-o-2tTFI5HBJbUvA=/0x0:1024x1024/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2018/t/P/kBI0LWRUemtdGTdIi9iQ/mussumdivulgacao.jpg',
-};
-
-const Home: NextPage = () => {
-  useEffect(() => {
-    const fetchExample = async () => {
-      const response = await fetch('/api/users/3ylBXptrDQxOV3M0hRuDlc');
-      const data = await response.json();
-      console.log(data);
-    };
-
-    fetchExample();
-  }, []);
-
-  return (
-    <>
-      <Header userName={mockUser.name} userPicturePath={mockUser.picture} />
-      <Sidebar />
-    </>
-  );
-=======
 import { useEffect, useState, useRef } from 'react';
 
 const Home: NextPage = () => {
@@ -44,6 +17,5 @@ const Home: NextPage = () => {
   }, []);
 
   return <Feed />;
->>>>>>> feature/createPost
 };
 export default Home;
