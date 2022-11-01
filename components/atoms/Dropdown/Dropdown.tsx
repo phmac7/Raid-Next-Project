4ae0b1d4351a0c3ab97f1bcf4c5dc0cd8120669f
KenjiWorkES <lucas.kenjies@gmail.com>
2022-11-01 14:34:38 -0300

    add keys to dropdown list

--- components/atoms/Dropdown/Dropdown.tsx
+++ components/atoms/Dropdown/Dropdown.tsx
@@ -6,7 +6,11 @@ const Dropdown: React.FC<DropdownType> = ({ id, name, options }) => {
   return (
     <select className={styles.dropdown} name={name}>
       {options.map((opt) => (
-        <option className={styles['dropdown__option']} value={opt.value}>
+        <option
+          key={opt.value}
+          className={styles['dropdown__option']}
+          value={opt.value}
+        >
           {opt.text}
         </option>
       ))}
