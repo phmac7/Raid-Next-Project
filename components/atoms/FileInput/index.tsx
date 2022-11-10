0743560962d6206cc0f499b28ceece57a791b5c9
KenjiWorkES <lucas.kenjies@gmail.com>
2022-11-10 14:47:56 -0300

    add the fileButton

--- components/atoms/FileInput/index.tsx
+++ components/atoms/FileInput/index.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from 'react';
 import { Camera } from 'phosphor-react';
+import styles from './fileInput.module.scss';
 
 const FileInput: FC = () => {
   return (
-    <div>
-      <Camera />
-      <label htmlFor="file">Photo</label>
-      <input id="file" type="file" />
+    <div className={styles.fileInput}>
+      <Camera size={28} />
+      <label className={styles['fileInput__label']} htmlFor="file">
+        Photo
+      </label>
+      <input id="file" className={styles['fileInput__field']} type="file" />
     </div>
   );
 };
