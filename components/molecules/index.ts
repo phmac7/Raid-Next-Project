2ea9d43605ab958a2671da94e58e5d2d7fc69e3e
Pedro Machado <pedro.machado@valtech.com>
2022-11-16 14:16:52 -0300

    fix sidebar link to profile

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -1,5 +1,5 @@
 export { default as SidebarItem } from './SidebarItem/SidebarItem';
-export { default as UserExpanded } from './UserExpanded/UserExpanded';
+export { default as UserTitle } from './UserTitle/UserTitle';
 export { default as UserInfo } from './UserInfo/UserInfo';
 export { default as UserGames } from './UserGames/UserGames';
 export { default as UserGameCard } from './UserGameCard/UserGameCard';
