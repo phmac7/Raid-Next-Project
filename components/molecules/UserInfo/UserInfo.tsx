9e89eb0caccb2b60a27e89f30ebcd1639889f151
Pedro Machado <pedro.machado@valtech.com>
2022-11-16 14:19:10 -0300

    and userInfos component

--- components/molecules/UserInfo/UserInfo.tsx
+++ components/molecules/UserInfo/UserInfo.tsx
@@ -1,18 +1,33 @@
-import { BasicInfo } from '@/components/atoms'
-import React from 'react'
-import styles from './UserInfo.module.scss'
+import { BasicInfo } from '@/components/atoms';
+import { User } from '@/models/contentfulObjects';
+import Image from 'next/image';
+import React from 'react';
+import styles from './UserInfo.module.scss';
 
-const UserInfo: React.FC = () => {
-    return (
-        <div className={styles.userInfo}>
-            <p className={styles.userInfo__title}>Basic Info</p>
-            <div className={styles.userInfo__details}>
-            <BasicInfo property={'Email'} value={'email.email@email.com'} />
-            <BasicInfo property={'date of bith'} value={'99/99/9999'} />
-
-            </div>
-        </div>
-    )
+interface UserInfoProps {
+  user: User;
 }
 
-export default UserInfo
+const UserInfo: React.FC<UserInfoProps> = ({ user }) => {
+  if (!user) {
+    return (
+      <div>
+        <Image src={'/assets/loading.gif'} width={500} height={500} />
+      </div>
+    );
+  }
+  return (
+    <div className={styles.userInfo}>
+      <p className={styles.userInfo__title}>Basic Info</p>
+      <div className={styles.userInfo__details}>
+        <BasicInfo property={'Email'} value={user.fields.email['en-US']} />
+        <BasicInfo
+          property={'Discord ID'}
+          value={user.fields.discord['en-US']}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
