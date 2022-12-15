0bb9502b97ee046ee1a002792d722ab2b213308d
andradeviniicius <viniciusdandrade01@gmail.com>
2022-12-15 08:47:20 -0300

    feat/ fetching the title of MyGames from contenful in <UserGameCard/>

--- components/molecules/UserGameCard/UserGameCard.tsx
+++ components/molecules/UserGameCard/UserGameCard.tsx
@@ -1,26 +1,24 @@
-import Image from 'next/image';
-import React from 'react';
-import styles from './UserGameCard.module.scss';
-import { GameCardProps } from '@/models/components';
+import Image from "next/image";
+import React from "react";
+import styles from "./UserGameCard.module.scss";
+import { GameCardProps } from "@/models/components";
 
 const UserGameCard: React.FC<GameCardProps> = ({ userGames }) => {
-  if (!userGames[0].fields.icon?.fields.file.url) {
-    return <div>Loading...</div>;
-  }
-
+  console.log(userGames);
   return (
     <div className={styles.card}>
       <div className={styles.card__heading}>
         <div className={styles.card__heading__img}>
           <Image
-            src={'https:' + userGames[0].fields.icon?.fields.file.url}
+            alt=""
+            src={""}
             width={80}
             height={80}
           />
         </div>
         <div className={styles.card__heading__text}>
           <p className={styles.card__heading__text__gameTitle}>
-            {userGames[0].fields.icon.fields.title}
+            {userGames.fields.name}
           </p>
           <div className={styles.card__heading__text__elo}>
             {/* <span className={styles.card__heading__text__eloText}>{elo}</span> */}
--- components/molecules/UserGames/UserGames.tsx
+++ components/molecules/UserGames/UserGames.tsx
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import React from 'react';
-import UserGameCard from '../UserGameCard/UserGameCard';
-import styles from './UserGames.module.scss';
-import { GameCardProps } from '@/models/components';
+import Link from "next/link";
+import React from "react";
+import UserGameCard from "../UserGameCard/UserGameCard";
+import styles from "./UserGames.module.scss";
+import { GameCardProps } from "@/models/components";
+import { Game } from "@/models/responseTypes";
 
 const UserGames: React.FC<GameCardProps> = ({ userGames }) => {
-  const gameValorant = userGames.filter(
-    (item) => item.fields.name === 'Valorant'
-  );
+  console.log(userGames);
+
   return (
     <div className={styles.userGames}>
       <div className={styles.userGames__heading}>
@@ -19,7 +19,9 @@ const UserGames: React.FC<GameCardProps> = ({ userGames }) => {
         </Link>
       </div>
       <div className={styles.userGames__games}>
-        <UserGameCard userGames={gameValorant} />
+        {userGames.map((el: any, i) => (
+          <UserGameCard key={i} userGames={el} />
+        ))}
       </div>
     </div>
   );
